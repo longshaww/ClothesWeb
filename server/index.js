dc288517a3afe63a75d84dda66459de634a3cec9
longshaww <at400123@gmail.com>
2022-03-02 05:19:49 +0700

    db deploy

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
 require("dotenv").config();
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 4000;
 
-const route = require("./routes/index")
-
-
+const route = require("./routes/index");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/ClothesWeb", {
+mongoose.connect(process.env.MONGO_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	user: process.env.MONGOUSER,
+	pass: process.env.MONGOPASSWORD,
+	dbName: "ClothesWeb",
 });
 
 app.use(express.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: false })) // support encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 // app.use("/public", express.static("public"));
 
 app.use(cors());
