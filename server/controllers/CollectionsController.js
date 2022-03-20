ff82e876198908fbb8d94f78df557b99d87747ac
longshaww <at400123@gmail.com>
2022-03-21 02:37:40 +0700

    api for android

--- server/controllers/CollectionsController.js
+++ server/controllers/CollectionsController.js
@@ -4,8 +4,17 @@ const pageSize = 12;
 class CollectionsController {
 	async getAllProduct(req, res, next) {
 		const selection = req.body.selection;
+		const pageNow = parseInt(req.query.page);
+		const skip = (pageNow - 1) * pageSize;
+		const newArrivals = await Products.find().skip(skip).limit(pageSize);
+		newArrivals
+			? res.status(202).json(newArrivals)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
 
-		// data fak
+	//[GET] collections without pagination
+	async getAllProductWithOutPagination(req, res, next) {
+		const selection = req.body.selection;
 		const newArrivals = await Products.find();
 		newArrivals
 			? res.status(202).json(newArrivals)
@@ -31,6 +40,18 @@ class CollectionsController {
 			: res.status(404).send("<h1>Không có dữ liệu</h1>");
 	}
 
+	//[GET] collections/new-arrivals without pagination
+
+	async getAllNewArrivalsWithOutPagination(req, res, next) {
+		const idFake = "621e4d91df99d34d865f9e55";
+		const newArrivals = await Products.find({
+			"description.collection": idFake,
+		});
+		newArrivals
+			? res.status(202).json(newArrivals)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
+
 	//[GET] collections/tops
 	async getAllTops(req, res, next) {
 		const pageNow = parseInt(req.query.page);
@@ -45,6 +66,18 @@ class CollectionsController {
 		})
 			.skip(skip)
 			.limit(pageSize);
+		tops
+			? res.status(202).json(tops)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
+
+	//[GET] collections/tops without pagination
+
+	async getAllTopsWithOutPag(req, res, next) {
+		const idFake = "621c506fbae8653bcb4564ac";
+		const tops = await Products.find({
+			"description.collection": idFake,
+		});
 		tops
 			? res.status(202).json(tops)
 			: res.status(404).send("<h1>Không có dữ liệu</h1>");
@@ -68,6 +101,17 @@ class CollectionsController {
 			: res.status(404).send("<h1>Không có dữ liệu</h1>");
 	}
 
+	//[GET] collections/bottoms without pagination
+	async getAllBottomsWithOutPag(req, res, next) {
+		const idFake = "621c50a7bae8653bcb4564b1";
+		const bottoms = await Products.find({
+			"description.collection": idFake,
+		});
+		bottoms
+			? res.status(202).json(bottoms)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
+
 	//[GET] collections/accessories
 	async getAllAccessories(req, res, next) {
 		const pageNow = parseInt(req.query.page);
@@ -86,6 +130,18 @@ class CollectionsController {
 			: res.status(404).send("<h1>Không có dữ liệu</h1>");
 	}
 
+	//[GET] collections/accessories without pagination
+
+	async getAllAccessoriesWithOutPag(req, res, next) {
+		const idFake = "621c50e0bae8653bcb4564b4";
+		const accessories = await Products.find({
+			"description.collection": idFake,
+		});
+		accessories
+			? res.status(202).json(accessories)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
+
 	//[GET] collections/Outerwears
 	async getAllOuterwears(req, res, next) {
 		const pageNow = parseInt(req.query.page);
@@ -99,6 +155,16 @@ class CollectionsController {
 		})
 			.skip(skip)
 			.limit(pageSize);
+		outerwears
+			? res.status(202).json(outerwears)
+			: res.status(404).send("<h1>Không có dữ liệu</h1>");
+	}
+
+	//[GET] collections/Outerwears without pagination
+	async getAllOuterwearsWithOutPagination() {
+		const outerwears = await Products.find({
+			"description.collection": idFake,
+		});
 		outerwears
 			? res.status(202).json(outerwears)
 			: res.status(404).send("<h1>Không có dữ liệu</h1>");
--- server/routes/collections.route.js
+++ server/routes/collections.route.js
@@ -3,11 +3,27 @@ const router = express.Router();
 
 const collectionsController = require("../controllers/CollectionsController");
 
-router.get("/", collectionsController.getAllProduct);
+router.get("/", collectionsController.getAllProductWithOutPagination);
 router.get("/new-arrivals", collectionsController.getAllNewArrivals);
+router.get(
+	"/new-arrivals-without-pag",
+	collectionsController.getAllNewArrivalsWithOutPagination
+);
 router.get("/tops", collectionsController.getAllTops);
+router.get("/tops-without-pag", collectionsController.getAllTopsWithOutPag);
 router.get("/bottoms", collectionsController.getAllBottoms);
+router.get(
+	"bottoms-without-pag",
+	collectionsController.getAllBottomsWithOutPag
+);
 router.get("/accessories", collectionsController.getAllAccessories);
+router.get(
+	"/accessories-without-pag",
+	collectionsController.getAllAccessoriesWithOutPag
+);
 router.get("/outerwears", collectionsController.getAllOuterwears);
-
+router.get(
+	"/outerwears-without-pag",
+	collectionsController.getAllOuterwearsWithOutPagination
+);
 module.exports = router;
