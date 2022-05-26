d4ea0af6cb8a03da2f6e5d385a61b2e32c1e1c8f
ngocphuphamm <ngocphupham682001@gmail.com>
2022-05-26 11:14:11 +0700

    adminroute

---
+++ server/controllers/AdminController.js
@@ -0,0 +1,41 @@
+const User = require("../models/User");
+const Bill = require("../models/Bills");
+const Product = require("../models/Product");
+class AdminController {
+    getDashboard(req,res,next)
+    {
+        
+    }
+    
+
+    async getBill(req,res,next)
+    {
+        const bill = await Bill.find({});
+        res.status(200).json({
+            success : true, 
+            bills:  bill 
+
+        });
+    }
+
+    async getUser(reqr,res,next)
+    {
+        const user = await User.find({});
+        res.status(200).json({
+            success : true , 
+            users : user
+        })
+        
+    }
+
+    async getProduct(req,res,next)
+    {
+        const product = await  Product.find({});
+        res.status(200).json({
+            "success" : true ,
+            products : product 
+        })
+    }
+}   
+
+module.exports = new AdminController();
