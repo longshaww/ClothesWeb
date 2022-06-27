a666b59ecd7901c6c9b9262c90efba49b8cfbbfd
longshaww <at400123@gmail.com>
2022-06-28 02:31:58 +0700

    fix cart

--- client/src/features/Collections/pages/collections.js
+++ client/src/features/Collections/pages/collections.js
@@ -1,8 +1,12 @@
 import renderDependOnCollection from "../../../components/productCard/product.card";
 import globalStateAndAction from "../../../container/global.state.action";
 import { collectionsEndpoint } from "../../../constants/constants";
+import { useEffect } from "react";
 
 function Collections({ collections }) {
+	useEffect(() => {
+		localStorage.removeItem("voucher");
+	}, []);
 	return (
 		<div id="collections">
 			{renderDependOnCollection(collections, collectionsEndpoint)}
