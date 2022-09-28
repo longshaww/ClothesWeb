5ef4bc8c6ab018fbd18659aa720b75818121fc9c
Ngọc Phú <ngocphupham682001@gmail.com>
2022-09-28 15:36:30 +0700

    morgan

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -3,11 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./../api/routes/index');
-
+const morgan = require('morgan');
 module.exports = (app) => {
     app.use(express.json()); // support json encoded bodies
     app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
-
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :date[clf]'));
     app.use(cors({ credentials: true, origin: true }));
     app.use(cookieParser(process.env.signed_cookie));
     app.use(express.static('public'));
