352597eed1d0c881f29ebf2725f5d98bcac48e5f
Ngọc Phú <ngocphupham682001@gmail.com>
2022-09-30 08:25:21 +0700

    product

--- server/src/api/services/user/collections/products.js
+++ server/src/api/services/user/collections/products.js
@@ -1,19 +1,17 @@
-const {SIZE_PAGE} = require('../../../../config/env');
-const ProductModel = require("../../../models/Product");
+const { SIZE_PAGE } = require('../../../../config/env');
+const ProductModel = require('../../../models/Product');
 const ObjectId = require('mongodb').ObjectId;
 const validator = require('../../../utils/validator');
 //  Creator
 
 class ProductsObject {
-    constructor(idCollection)
-    {
+    constructor(idCollection) {
         this.pageSize = parseInt(SIZE_PAGE);
         this.idCollection = idCollection;
     }
 
-    async findProducts(pageNow)
-    {
-        try{
+    async findProducts(pageNow) {
+        try {
             const skip = (pageNow - 1) * this.pageSize;
             const listProducts = await ProductModel.find({
                 'description.collection': ObjectId(this.idCollection),
@@ -21,12 +19,17 @@ class ProductsObject {
                 .skip(skip)
                 .limit(this.pageSize);
             return listProducts ?? null;
+        } catch (err) {
+            return null;
         }
-        catch(err)
-        {
-            return err.message;
+    }
+    async getProductDetail() {
+        try {
+            const product = await ProductModel.findById(this.idCollection);
+            return product ?? null;
+        } catch (err) {
+            return null;
         }
-     
-    } 
+    }
 }
-module.exports =  ProductsObject;
+module.exports = ProductsObject;
