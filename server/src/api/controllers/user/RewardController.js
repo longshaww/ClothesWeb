cb41a3574efd4490c207166a347e049d6c338ab5
longshaww <at400123@gmail.com>
2022-10-15 19:07:27 +0700

    exchange voucher,list available items for exchange feature done

---
+++ server/src/api/controllers/user/RewardController.js
@@ -0,0 +1,43 @@
+const { successRes, throwErr } = require('../../utils/HandleResponse');
+const rewardService = require('../../services/user/reward');
+class RewardController {
+    async availableForExchange(req, res) {
+        try {
+            const { id } = req.params;
+            if (!id) {
+                return throwErr(res, 403, 'Forbidden');
+            }
+            const data = await rewardService.availableForExchange(id);
+            if (typeof data === 'string') {
+                return throwErr(res, 400, data);
+            }
+            if (!data) {
+                return throwErr(res, 400, 'There is no available item for exchange');
+            }
+            return successRes(res, 200, data, 'Successfully get exchange items');
+        } catch (err) {
+            throwErr(res, 400, err.message);
+        }
+    }
+    async exchangeVoucher(req, res) {
+        try {
+            const { userID, voucherID } = req.body;
+            if (!userID && !voucherID) {
+                return throwErr(res, 403, 'Forbidden');
+            }
+            const data = await rewardService.exchangeVoucher(userID, voucherID);
+            if (typeof data === 'string') {
+                return throwErr(res, 400, data);
+            }
+            return successRes(
+                res,
+                200,
+                `You achieve a voucher with discount ${data.discount}. This voucher is automatically added to your own voucher list`
+            );
+        } catch (err) {
+            throwErr(res, 400, err.message);
+        }
+    }
+}
+
+module.exports = new RewardController();
--- server/src/api/routes/user/user.route.js
+++ server/src/api/routes/user/user.route.js
@@ -1,7 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../../controllers/user/UserController");
-router.get("/getUser/:id",userController.getUser);
-router.put("/editUser/:id",userController.editUser);
-router.get("/getBillUser/:id",userController.getBillUser);
+const userController = require('../../controllers/user/UserController');
+const rewardController = require('../../controllers/user/RewardController');
+
+router.get('/getUser/:id', userController.getUser);
+router.put('/editUser/:id', userController.editUser);
+router.get('/getBillUser/:id', userController.getBillUser);
+router.get('/availableForExchange/:id', rewardController.availableForExchange);
+router.post('/exchangeVoucher/', rewardController.exchangeVoucher);
+
 module.exports = router;
