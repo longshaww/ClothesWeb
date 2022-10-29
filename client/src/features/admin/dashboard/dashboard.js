670d55c617d54f7b922670d57955793766b83481
longshaww <at400123@gmail.com>
2022-10-30 02:08:54 +0700

    apply mockup for dashboard

--- client/src/features/admin/dashboard/dashboard.js
+++ client/src/features/admin/dashboard/dashboard.js
@@ -1,36 +1,39 @@
-import Chart from "../dashboard/chart"
-import FeaturedInfo from "../dashboard/featuredInfo";
-import "../../../assets/styles/admin/home.css"
+import Chart from '../dashboard/chart';
+import FeaturedInfo from '../dashboard/featuredInfo';
+import '../../../assets/styles/admin/home.css';
 import globalStateAndAction from '../../../container/global.state.action';
 import { useEffect } from 'react';
-import { useCookies } from "react-cookie";
-import axios from "axios";
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 import { useState } from 'react';
-function Dashboard({ setDataDashBoard }) {
-	const [cookies] = useCookies(["accessToken"]);
-	const [lineChartBill,setLineChartBill] = useState([]);
-	
-	useEffect(() => {
-		const getData = async () => {
-			const endpoint = `${process.env.REACT_APP_API_URL}admin/dashboard/getDashBoard`
-			const { data } = await axios.get(endpoint,
-				{
-					headers: {
-						authorization:
-							"Bearer " + cookies.accessToken,
-					},
-				});
-			setDataDashBoard(data.data)
-			setLineChartBill(data.data.bills.lineChartBill)
-		};
-		getData();
-	}, [cookies.accessToken,setDataDashBoard]);
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
-	return (
-		<div className="home mt-5">
-			<FeaturedInfo />
-			<Chart data={lineChartBill} title="Money Analytics" grid dataKey="TOTAL" />
-		</div>
-	);
+function Dashboard({ setDataDashBoard }) {
+    const [cookies] = useCookies(['accessToken']);
+    const [lineChartBill, setLineChartBill] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const endpoint = `${process.env.REACT_APP_API_URL}admin/dashboard/getDashBoard`;
+            const { data } = await axios.get(endpoint, {
+                headers: {
+                    authorization: 'Bearer ' + cookies.accessToken,
+                },
+            });
+            setDataDashBoard(data.data);
+            setLineChartBill(data.data.bills.lineChartBill);
+        };
+        getData();
+    }, [cookies.accessToken, setDataDashBoard]);
+
+    return (
+        <div className="home mt-5">
+            <Stack m={5} spacing={4}>
+                <FeaturedInfo />
+                <Chart data={lineChartBill} title="Money Analytics" grid dataKey="TOTAL" />
+            </Stack>
+        </div>
+    );
 }
-export default globalStateAndAction(Dashboard)
+export default globalStateAndAction(Dashboard);
--- client/src/features/admin/dashboard/featuredInfo.js
+++ client/src/features/admin/dashboard/featuredInfo.js
@@ -6,6 +6,14 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import moment from 'moment';
 import { TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
 
 function FeaturedInfo({ listDashBoard }) {
     const [infoQty, setInfoQty] = useState({
@@ -13,7 +21,12 @@ function FeaturedInfo({ listDashBoard }) {
         qtyBill: 0,
         qtyUser: 0,
     });
-    const [date, setDate] = useState({ startDate: new Date(), endDate: new Date() });
+    const [filter, setFilter] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        billState: 'SUCCESSFUL_DELIVERY_CONFIRMATION',
+        paymentMethod: 'COD',
+    });
 
     useEffect(() => {
         setInfoQty(() => ({
@@ -22,49 +35,142 @@ function FeaturedInfo({ listDashBoard }) {
             qtyUser: listDashBoard.qtyUser,
         }));
     }, [listDashBoard]);
+
+    const paymentMethodOptions = [
+        {
+            value: 'COD',
+            label: 'Thanh toán khi nhận hàng',
+        },
+        {
+            value: 'Online',
+            label: 'Thanh toán trực tuyến',
+        },
+    ];
+    const billStateOptions = [
+        {
+            value: 'PENDING',
+            label: 'Chưa duyệt',
+        },
+        {
+            value: 'DELIVERY',
+            label: 'Đang giao',
+        },
+        {
+            value: 'SUCCESSFUL_DELIVERY_CONFIRMATION',
+            label: 'Đã giao',
+        },
+        {
+            value: 'FAILED_DELIVERY_CONFIRMATION',
+            label: 'Giao thất bại',
+        },
+        {
+            value: 'FAILED_DELIVERY_CONFIRMATION',
+            label: 'Giao thất bại',
+        },
+        {
+            value: 'CANCEL_BILL',
+            label: 'Hủy đơn',
+        },
+    ];
     return (
         <>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-                <DatePicker
-                    label="Ngày bắt đầu"
-                    inputFormat="DD/MM/YYYY"
-                    value={date.startDate}
-                    // onChange={handleDateEndChange}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-            </LocalizationProvider>{' '}
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-                <DatePicker
-                    label="Ngày kết thúc"
-                    inputFormat="DD/MM/YYYY"
-                    value={date.endDate}
-                    // onChange={handleDateEndChange}
-                    renderInput={(params) => <TextField {...params} />}
-                />
-            </LocalizationProvider>
-            <div className="featured">
-                <div className="featuredItem">
-                    <span className="featuredTitle">TỔNG SẢN PHẨM</span>
-                    <div className="featuredMoneyContainer">
-                        <span className="featuredMoney">{infoQty.qtyProduct}</span>
-                    </div>
-                    <span className="featuredSub">SẢN PHẨM</span>
-                </div>
-                <div className="featuredItem">
-                    <span className="featuredTitle">TỔNG ĐƠN TRONG THÁNG</span>
-                    <div className="featuredMoneyContainer">
-                        <span className="featuredMoney">{infoQty.qtyBill}</span>
-                    </div>
-                    <span className="featuredSub">ĐƠN HÀNG</span>
-                </div>
-                <div className="featuredItem">
-                    <span className="featuredTitle">TỔNG KHÁCH HÀNG</span>
-                    <div className="featuredMoneyContainer">
-                        <span className="featuredMoney">{infoQty.qtyUser}</span>
-                    </div>
-                    <span className="featuredSub">KHÁCH HÀNG</span>
-                </div>
-            </div>
+            <Box p={5} sx={{ boxShadow: 1, borderRadius: 1 }}>
+                <Stack spacing={3}>
+                    <FormControl fullWidth>
+                        <InputLabel id="billState">Lọc theo trạng thái</InputLabel>
+                        <Select
+                            labelId="billState"
+                            id="billState"
+                            value={filter.billState}
+                            label="Lọc theo trạng thái"
+                            onChange={(e) => setFilter({ ...filter, billState: e.target.value })}
+                        >
+                            {billStateOptions.map((state) => (
+                                <MenuItem value={state.value}>{state.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth>
+                        <InputLabel id="paymentMethod">Hình thức thanh toán</InputLabel>
+                        <Select
+                            labelId="paymentMethod"
+                            id="paymentMethod"
+                            value={filter.paymentMethod}
+                            label="Hình thức thanh toán"
+                            onChange={(e) =>
+                                setFilter({ ...filter, paymentMethod: e.target.value })
+                            }
+                        >
+                            {paymentMethodOptions.map((payment) => (
+                                <MenuItem value={payment.value}>{payment.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Box
+                        sx={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(2, 1fr)',
+                            gap: 3,
+                        }}
+                    >
+                        <LocalizationProvider dateAdapter={AdapterMoment}>
+                            <DatePicker
+                                label="Ngày bắt đầu"
+                                inputFormat="DD/MM/YYYY"
+                                value={filter.startDate}
+                                // onChange={handleDateEndChange}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                        <LocalizationProvider dateAdapter={AdapterMoment}>
+                            <DatePicker
+                                label="Ngày kết thúc"
+                                inputFormat="DD/MM/YYYY"
+                                value={filter.endDate}
+                                // onChange={handleDateEndChange}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </Box>
+                </Stack>
+            </Box>
+            <Box>
+                <Grid container spacing={3}>
+                    <Grid xs={4}>
+                        <Box p={5} sx={{ borderRadius: 1, boxShadow: 1 }}>
+                            <Stack spacing={1}>
+                                <Typography fontSize={22}>Tổng sản phẩm</Typography>
+                                <Typography fontWeight="bold" fontSize={20}>
+                                    {infoQty.qtyProduct}
+                                </Typography>
+                                <Typography variant="subtitle1">SẢN PHẨM</Typography>
+                            </Stack>
+                        </Box>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Box p={5} sx={{ borderRadius: 1, boxShadow: 1 }}>
+                            <Stack spacing={1}>
+                                <Typography fontSize={22}>Tổng sản phẩm</Typography>
+                                <Typography fontWeight="bold" fontSize={20}>
+                                    {infoQty.qtyProduct}
+                                </Typography>
+                                <Typography variant="subtitle1">SẢN PHẨM</Typography>
+                            </Stack>
+                        </Box>
+                    </Grid>
+                    <Grid xs={4}>
+                        <Box p={5} sx={{ borderRadius: 1, boxShadow: 1 }}>
+                            <Stack spacing={1}>
+                                <Typography fontSize={22}>Tổng sản phẩm</Typography>
+                                <Typography fontWeight="bold" fontSize={20}>
+                                    {infoQty.qtyProduct}
+                                </Typography>
+                                <Typography variant="subtitle1">SẢN PHẨM</Typography>
+                            </Stack>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
         </>
     );
 }
