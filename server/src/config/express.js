02b298e16f0b0c4a4f1e8bcda6994c5a251a0da6
long.tran <long.tran@zien.vn>
2022-11-22 14:57:13 +0700

    cors policy

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     app.use(express.json()); // support json encoded bodies
     app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
     app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :date[clf]'));
-    app.use(cors({ credentials: true, origin: true }));
+    app.use(cors());
     app.use(cookieParser(process.env.signed_cookie));
     app.use(express.static(path.join(__dirname, '../api/public')));
     app.use(compression());
