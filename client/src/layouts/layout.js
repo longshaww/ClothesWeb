364ff0470962b70dfdfd4922c02ea0e3bdbaea18
longshaww <at400123@gmail.com>
2022-12-08 00:31:12 +0700

    remove go up button

--- client/src/layouts/layout.js
+++ client/src/layouts/layout.js
@@ -1,7 +1,6 @@
 import NavbarApp from '../components/navbars/navbar/navbar';
 import Footer from '../components/footer/footer';
 import { Outlet } from 'react-router-dom';
-import GoToTop from '../components/GoToTopButton/go.to.top';
 import '../assets/styles/layout.css';
 import { useSelector } from 'react-redux';
 export default function Layout() {
@@ -10,13 +9,12 @@ export default function Layout() {
     return (
         <div className="m-main">
             <NavbarApp />
-            <GoToTop />
             <div className="m-article" style={{ marginTop: `${height}px` }}>
                 <Outlet />
             </div>
             <div className="footer-design">
                 <Footer />
-            </div>{' '}
+            </div>
         </div>
     );
 }
